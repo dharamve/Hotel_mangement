6fc2c22f010c71d92c66c7d8a5d37a2af68bb976
dharamve <103619968+dharamve@users.noreply.github.com>
2023-08-31 22:49:32 +0530

    Add files via upload

---
+++ mainly.py
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>SRM CAMPUS GUIDE</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='style.css'>
+</head>
+
+<body>
+    <div class="container">
+        <h1>
+            <a href="#menu">SRM CAMPUS GUIDE</a>
+        </h1>
+
+        <div class="popover" id="menu">
+            <div class="content">
+                <a href="#" class="close"></a>
+                <nav class="nav">
+                    <ul class="nav_list">
+                        <div class="nav_list_item">
+                            <li><a href="home.html">Home</a></li>
+                        </div>
+
+                        <div class="nav_list_item">
+                            <li><a href="about.html">About</a></li>
+                        </div>
+
+                        <div class="nav_list_item">
+                            <li><a href="Untitled-1.html">Map</a></li>
+                        </div>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
